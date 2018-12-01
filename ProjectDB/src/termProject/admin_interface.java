2cb843fd40755dce3ad3135aa73e9d441b526c6b
yun <dbs1597@naver.com>
2018-12-01 14:56:29 +0900

    test

---
+++ ProjectDB/src/termProject/admin_interface.java
@@ -0,0 +1,28 @@
+package termProject;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.sql.*;
+import javax.swing.*;
+import java.util.Calendar;
+
+//admin 계정으로 로그인시 띄우는 화면
+public class admin_interface extends JFrame implements ActionListener{
+	
+	public admin_interface() {
+		
+		setSize(1000, 600);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
