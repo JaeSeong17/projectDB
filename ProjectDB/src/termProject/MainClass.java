59cd93b28060ea588c8d08b2bc498cc8009ec0d1
JaeSeong17 <dktmzh6@gmail.com>
2018-12-01 15:28:18 +0900

    mysql library path 수정
    라이브러리 경로 수정

--- ProjectDB/src/termProject/MainClass.java
+++ ProjectDB/src/termProject/MainClass.java
@@ -2,9 +2,9 @@ package termProject;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
+//import java.sql.Statement;
+//import java.sql.ResultSet;
 
 public class MainClass {
 	public static void main(String[] args) {
